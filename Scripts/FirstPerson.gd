86e3f836ad1358e343f6348555fb052e71d26176
Mylloon <kennel.anri@tutanota.com>
2025-08-02 22:50:01 +0200

    better control support (zqsd and derivatives, controller)

--- Scripts/FirstPerson.gd
+++ Scripts/FirstPerson.gd
@@ -5,6 +5,8 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const CAMERA_SENS = 0.003
+const CONTROLLER_SENS = 2.0
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -17,10 +19,23 @@ func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 
+	# Control with mouse
 	if event is InputEventMouseMotion:
 		rotation.y -= event.relative.x * CAMERA_SENS
 		rotation.x -= event.relative.y * CAMERA_SENS
 		rotation.x = clamp(rotation.x, -0.5, 1.2)
+
+
+func _process(delta):
+	# Control with controller (joke)
+	var look_input = Vector2()
+	look_input.x = Input.get_action_strength("look_right") - Input.get_action_strength("look_left")
+	look_input.y = Input.get_action_strength("look_down") - Input.get_action_strength("look_up")
+
+	# We don't fuck with deadzones as Godot probably(?) handle it
+	rotation.y -= look_input.x * CONTROLLER_SENS * delta
+	rotation.x -= look_input.y * CONTROLLER_SENS * delta
+	rotation.x = clamp(rotation.x, -0.5, 1.2)
 
 
 func _physics_process(delta):
@@ -29,13 +44,11 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	# TODO : remplacer les flèches par ZQSD (ou en tout cas les touches pour se déplacer)
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
