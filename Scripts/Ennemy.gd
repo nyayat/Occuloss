d7def57a42ef84c52977c85552f0afa6b610c1f3
2-1-1-2 <tanyan.francois@gmail.com>
2025-08-04 00:05:00 +0200

    ✨feature: weeping angels stops with light + vision

--- Scripts/Ennemy.gd
+++ Scripts/Ennemy.gd
@@ -3,19 +3,62 @@ extends CharacterBody3D
 @export var speed: float = 5.0
 @export var player: CharacterBody3D
 @export var camera: Camera3D
+@export var spot_light: SpotLight3D
 
 var agent: NavigationAgent3D
 var has_teleported := false
 var teleport_pause := false
 var teleport_delay := 2.5  # seconds to wait after teleport
 var teleport_timer := 0.0
+var visible_lights: Array[SpotLight3D] = []
 
+func get_all_children(in_node, array := []):
+	array.push_back(in_node)
+	for child in in_node.get_children():
+		array = get_all_children(child, array)
+	return array
 
 func _ready():
 	agent = $NavigationAgent3D
 	agent.target_position = global_transform.origin  # Stay still initially
 	#player = get_node("FPPlayer")
 	#camera = get_node("Camera3D")
+	
+	for element in get_all_children(get_tree().get_root()):
+		if element is SpotLight3D:
+			visible_lights.append(element)
+
+
+func is_under_any_spotlight() -> bool:
+	for light in visible_lights:
+		if is_under_spotlight(light):
+			return true
+	return false
+
+func is_under_spotlight(light: SpotLight3D) -> bool:
+	var light_pos = light.global_transform.origin
+	var light_dir = -light.global_transform.basis.z.normalized()
+	var to_enemy = global_transform.origin - light_pos
+	var distance = to_enemy.length()
+
+	if distance > light.spot_range:
+		return false
+
+	var angle_cos = light_dir.dot(to_enemy.normalized())
+	var limit_cos = cos(deg_to_rad(light.spot_angle))
+	if angle_cos < limit_cos:
+		return false
+
+	var space_state = get_world_3d().direct_space_state
+	var ray = PhysicsRayQueryParameters3D.create(light_pos, global_transform.origin)
+	ray.exclude = [self, light]
+	var result = space_state.intersect_ray(ray)
+
+	if result.is_empty() or result["collider"] == self:
+		return true
+
+	return false
+
 
 
 func _physics_process(delta):
@@ -23,22 +66,20 @@ func _physics_process(delta):
 		teleport_timer -= delta
 		if teleport_timer <= 0.0:
 			teleport_pause = false
-			print("j'arrive")
 	else:
-		if not is_seen_by_camera():
+		if (is_seen_by_camera() and is_under_any_spotlight()):
+			velocity = Vector3.ZERO
+			move_and_slide()
+			has_teleported = false
+		else : 
 			if not has_teleported:
-				print("je me suis téléporté")
 				teleport_around_player()
 				has_teleported = true
 				teleport_pause = true
 				teleport_timer = teleport_delay
 			agent.target_position = player.global_transform.origin
 			move_along_path(delta)
-		else:
-			print("STOP")
-			velocity = Vector3.ZERO
-			move_and_slide()
-			has_teleported = false
+			
 
 
 func caught():
