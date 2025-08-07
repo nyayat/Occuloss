f898c56098e8095da665198b1def94ecc5177df1
Mylloon <kennel.anri@tutanota.com>
2025-08-07 17:46:03 +0200

    ✨fix(resize): use scaling

--- Scripts/PlayerFilter.gd
+++ Scripts/PlayerFilter.gd
@@ -2,13 +2,4 @@ extends ColorRect
 
 
 func _ready():
-	update_size()
-
-
-func _notification(what):
-	if what == NOTIFICATION_RESIZED:
-		update_size()
-
-
-func update_size():
 	size = get_viewport().size
